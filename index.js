a2ec36cae02ce5c7a96a1154358db867e0f065c0
Tobias Gurtzick <magic@wizardtales.com>
2019-12-11 11:33:12 +0100

    chore(cs): fix codestyle
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- index.js
+++ index.js
@@ -9,51 +9,43 @@ var type;
 var internals = {};
 
 var MysqlDriver = Base.extend({
-  init: function(connection) {
+  init: function (connection) {
     this._escapeDDL = '`';
-    this._escapeString = '\'';
+    this._escapeString = "'";
     this._super(internals);
     this.connection = connection;
   },
 
-  startMigration: function(cb){
-
+  startMigration: function (cb) {
     var self = this;
 
-    if(!internals.notransactions) {
-
+    if (!internals.notransactions) {
       return this.runSql('SET AUTOCOMMIT=0;')
-             .then(function() {
-                return self.runSql('START TRANSACTION;');
-            })
-             .nodeify(cb);
-    }
-    else
-      return Promise.resolve().nodeify(cb);
+        .then(function () {
+          return self.runSql('START TRANSACTION;');
+        })
+        .nodeify(cb);
+    } else return Promise.resolve().nodeify(cb);
   },
 
-  endMigration: function(cb){
-
-    if(!internals.notransactions) {
-
+  endMigration: function (cb) {
+    if (!internals.notransactions) {
       return this.runSql('COMMIT;').nodeify(cb);
-    }
-    else
-      return Promise.resolve(null).nodeify(cb);
+    } else return Promise.resolve(null).nodeify(cb);
   },
 
-  mapDataType: function(spec) {
+  mapDataType: function (spec) {
     var len;
-    switch(spec.type) {
+    switch (spec.type) {
       case type.TEXT:
         len = parseInt(spec.length, 10) || 1000;
-        if(len > 16777216) {
+        if (len > 16777216) {
           return 'LONGTEXT';
         }
-        if(len > 65536) {
+        if (len > 65536) {
           return 'MEDIUMTEXT';
         }
-        if(len > 256) {
+        if (len > 256) {
           return 'TEXT';
         }
         return 'TINYTEXT';
@@ -61,13 +53,13 @@ var MysqlDriver = Base.extend({
         return 'DATETIME';
       case type.BLOB:
         len = parseInt(spec.length, 10) || 1000;
-        if(len > 16777216) {
+        if (len > 16777216) {
           return 'LONGBLOB';
         }
-        if(len > 65536) {
+        if (len > 65536) {
           return 'MEDIUMBLOB';
         }
-        if(len > 256) {
+        if (len > 256) {
           return 'BLOB';
         }
         return 'TINYBLOB';
@@ -79,27 +71,33 @@ var MysqlDriver = Base.extend({
     return this._super(spec.type);
   },
 
-  createColumnDef: function(name, spec, options, tableName) {
-    var escapedName = util.format('`%s`', name),
-        t = this.mapDataType(spec),
-        len;
-    if(spec.type === type.DECIMAL) {
-       if(spec.precision && spec.scale){
-         len = '(' + spec.precision + ',' + spec.scale + ')';
-       }
-    }
-    else if(spec.type !== type.TEXT && spec.type !== type.BLOB) {
+  createColumnDef: function (name, spec, options, tableName) {
+    var escapedName = util.format('`%s`', name);
+    var t = this.mapDataType(spec);
+    var len;
+    if (spec.type === type.DECIMAL) {
+      if (spec.precision && spec.scale) {
+        len = '(' + spec.precision + ',' + spec.scale + ')';
+      }
+    } else if (spec.type !== type.TEXT && spec.type !== type.BLOB) {
       len = spec.length ? util.format('(%s)', spec.length) : '';
       if (t === 'VARCHAR' && len === '') {
         len = '(255)';
       }
     }
-    var constraint = this.createColumnConstraint(spec, options, tableName, name);
-    return { foreignKey: constraint.foreignKey,
-             constraints: [escapedName, t, len, constraint.constraints].join(' ') };
+    var constraint = this.createColumnConstraint(
+      spec,
+      options,
+      tableName,
+      name
+    );
+    return {
+      foreignKey: constraint.foreignKey,
+      constraints: [escapedName, t, len, constraint.constraints].join(' ')
+    };
   },
 
-  createColumnConstraint: function(spec, options, tableName, columnName) {
+  createColumnConstraint: function (spec, options, tableName, columnName) {
     var constraint = [];
     var cb;
 
@@ -125,16 +123,16 @@ var MysqlDriver = Base.extend({
       constraint.push('UNIQUE');
     }
 
-    if (spec.engine && typeof(spec.engine) === 'string') {
-      constraint.push('ENGINE=\'' + spec.engine + '\'')
+    if (spec.engine && typeof spec.engine === 'string') {
+      constraint.push("ENGINE='" + spec.engine + "'");
     }
 
-    if (spec.rowFormat && typeof(spec.rowFormat) === 'string') {
-      constraint.push('ROW_FORMAT=\'' + spec.rowFormat + '\'')
+    if (spec.rowFormat && typeof spec.rowFormat === 'string') {
+      constraint.push("ROW_FORMAT='" + spec.rowFormat + "'");
     }
 
     if (spec.onUpdate && spec.onUpdate.startsWith('CURRENT_TIMESTAMP')) {
-      constraint.push('ON UPDATE ' + spec.onUpdate)
+      constraint.push('ON UPDATE ' + spec.onUpdate);
     }
 
     if (spec.null || spec.notNull === false) {
@@ -144,11 +142,10 @@ var MysqlDriver = Base.extend({
     if (spec.defaultValue !== undefined) {
       constraint.push('DEFAULT');
 
-      if (typeof spec.defaultValue === 'string'){
-        if(spec.defaultValue.startsWith('CURRENT_TIMESTAMP')) {
+      if (typeof spec.defaultValue === 'string') {
+        if (spec.defaultValue.startsWith('CURRENT_TIMESTAMP')) {
           constraint.push(spec.defaultValue);
-        }
-        else {
+        } else {
           constraint.push("'" + spec.defaultValue + "'");
         }
       } else if (spec.defaultValue === null) {
@@ -159,76 +156,70 @@ var MysqlDriver = Base.extend({
     }
 
     if (spec.comment) {
-        constraint.push('COMMENT \'' + spec.comment + '\'')
+      constraint.push("COMMENT '" + spec.comment + "'");
     }
 
     if (spec.foreignKey) {
-
       cb = this.bindForeignKey(tableName, columnName, spec.foreignKey);
     }
 
     return { foreignKey: cb, constraints: constraint.join(' ') };
   },
 
-
-  renameTable: function(tableName, newTableName, callback) {
+  renameTable: function (tableName, newTableName, callback) {
     var sql = util.format('RENAME TABLE `%s` TO `%s`', tableName, newTableName);
     return this.runSql(sql).nodeify(callback);
   },
 
-  createDatabase: function(dbName, options, callback) {
+  createDatabase: function (dbName, options, callback) {
+    var spec = '';
+    var ifNotExists = '';
 
-    var spec = '',
-        ifNotExists = '';
-
-    if(typeof(options) === 'function')
-      callback = options;
-    else
-    {
-      ifNotExists = (options.ifNotExists === true) ? 'IF NOT EXISTS' : '';
+    if (typeof options === 'function') callback = options;
+    else {
+      ifNotExists = options.ifNotExists === true ? 'IF NOT EXISTS' : '';
     }
 
-    this.runSql(util.format('CREATE DATABASE %s `%s` %s', ifNotExists, dbName, spec), callback);
+    this.runSql(
+      util.format('CREATE DATABASE %s `%s` %s', ifNotExists, dbName, spec),
+      callback
+    );
   },
 
-  switchDatabase: function(options, callback) {
-
-    if(typeof(options) === 'object')
-    {
-      if(typeof(options.database) === 'string')
-
-        this.all(util.format('USE `%s`', options.database), callback);
-    }
-    else if(typeof(options) === 'string')
-    {
+  switchDatabase: function (options, callback) {
+    if (typeof options === 'object') {
+      if (typeof options.database === 'string') { this.all(util.format('USE `%s`', options.database), callback); }
+    } else if (typeof options === 'string') {
       this.all(util.format('USE `%s`', options), callback);
-    }
-    else
-      callback(null);
+    } else callback(null);
   },
 
-  dropDatabase: function(dbName, options, callback) {
-
+  dropDatabase: function (dbName, options, callback) {
     var ifExists = '';
 
-    if(typeof(options) === 'function')
-      callback = options;
-    else
-    {
-      ifExists = (options.ifExists === true) ? 'IF EXISTS' : '';
+    if (typeof options === 'function') callback = options;
+    else {
+      ifExists = options.ifExists === true ? 'IF EXISTS' : '';
     }
 
-    this.runSql(util.format('DROP DATABASE %s `%s`', ifExists, dbName), callback);
+    this.runSql(
+      util.format('DROP DATABASE %s `%s`', ifExists, dbName),
+      callback
+    );
   },
 
-  removeColumn: function(tableName, columnName, callback) {
-    var sql = util.format('ALTER TABLE `%s` DROP COLUMN `%s`', tableName, columnName);
+  removeColumn: function (tableName, columnName, callback) {
+    var sql = util.format(
+      'ALTER TABLE `%s` DROP COLUMN `%s`',
+      tableName,
+      columnName
+    );
 
     return this.runSql(sql).nodeify(callback);
   },
 
-  addIndex: function(tableName, indexName, columns, unique, callback) {
-    if (typeof(unique) === 'function') {
+  addIndex: function (tableName, indexName, columns, unique, callback) {
+    if (typeof unique === 'function') {
       callback = unique;
       unique = false;
     }
@@ -242,30 +233,40 @@ var MysqlDriver = Base.extend({
       var column = columns[columnIndex];
       var columnSpec = '';
 
-      if (typeof(column) === 'object' && column.name) {
-        columnSpec = util.format('`%s`%s',
-                column.name,
-                (column.length ? util.format('(%s)', parseInt(column.length)) : '')
+      if (typeof column === 'object' && column.name) {
+        columnSpec = util.format(
+          '`%s`%s',
+          column.name,
+          column.length ? util.format('(%s)', parseInt(column.length)) : ''
         );
-      } else if (typeof(column) === 'string') {
+      } else if (typeof column === 'string') {
         columnSpec = util.format('`%s`', column);
-      } else
-        return callback(new Error('Invalid column specification'));
+      } else return callback(new Error('Invalid column specification'));
 
       columnsList.push(columnSpec);
     }
 
-    var sql = util.format('ALTER TABLE `%s` ADD %s INDEX `%s` (%s)', tableName, (unique ? 'UNIQUE ' : ''), indexName, columnsList.join(', '));
+    var sql = util.format(
+      'ALTER TABLE `%s` ADD %s INDEX `%s` (%s)',
+      tableName,
+      unique ? 'UNIQUE ' : '',
+      indexName,
+      columnsList.join(', ')
+    );
     return this.runSql(sql).nodeify(callback);
   },
 
-  removeIndex: function(tableName, indexName, callback) {
+  removeIndex: function (tableName, indexName, callback) {
     // tableName is optional for other drivers, but required for mySql.
     // So, check the args to ensure they are valid
-    if (arguments.length === 2 && typeof(indexName) === 'function') {
+    if (arguments.length === 2 && typeof indexName === 'function') {
       callback = indexName;
       process.nextTick(function () {
-        callback(new Error('Illegal arguments, must provide "tableName" and "indexName"'));
+        callback(
+          new Error(
+            'Illegal arguments, must provide "tableName" and "indexName"'
+          )
+        );
       });
 
       return;
@@ -276,114 +277,156 @@ var MysqlDriver = Base.extend({
     return this.runSql(sql).nodeify(callback);
   },
 
-  renameColumn: function(tableName, oldColumnName, newColumnName, callback) {
-    var self = this, columnTypeSql = util.format("SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '%s' AND COLUMN_NAME = '%s'", tableName, oldColumnName);
+  renameColumn: function (tableName, oldColumnName, newColumnName, callback) {
+    var self = this;
+    var columnTypeSql = util.format(
+      "SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '%s' AND COLUMN_NAME = '%s'",
+      tableName,
+      oldColumnName
+    );
 
     return this.runSql(columnTypeSql)
-    .then(function(result) {
-      var columnType = result[0].COLUMN_TYPE;
-      var alterSql = util.format("ALTER TABLE `%s` CHANGE `%s` `%s` %s", tableName, oldColumnName, newColumnName, columnType);
+      .then(function (result) {
+        var columnType = result[0].COLUMN_TYPE;
+        var alterSql = util.format(
+          'ALTER TABLE `%s` CHANGE `%s` `%s` %s',
+          tableName,
+          oldColumnName,
+          newColumnName,
+          columnType
+        );
 
-      return self.runSql(alterSql);
-    }).nodeify(callback);
+        return self.runSql(alterSql);
+      })
+      .nodeify(callback);
   },
 
-  changeColumn: function(tableName, columnName, columnSpec, callback) {
+  changeColumn: function (tableName, columnName, columnSpec, callback) {
     var constraint = this.createColumnDef(columnName, columnSpec);
-    var sql = util.format('ALTER TABLE `%s` CHANGE COLUMN `%s` %s', tableName, columnName, constraint.constraints);
+    var sql = util.format(
+      'ALTER TABLE `%s` CHANGE COLUMN `%s` %s',
+      tableName,
+      columnName,
+      constraint.constraints
+    );
 
-    var exec = function() {
-
-      return this.runSql(sql)
-      .then(function()
-      {
-        if(constraint.foreignKey)
-          return constraint.foreignKey();
-        else
-          return Promise.resolve();
+    var exec = function () {
+      return this.runSql(sql).then(function () {
+        if (constraint.foreignKey) return constraint.foreignKey();
+        else return Promise.resolve();
       });
     }.bind(this);
 
-    if(columnSpec.unique === false)
+    if (columnSpec.unique === false) {
       return this.removeIndex(tableName, columnName)
-      .then(function() {
-        return exec();
-      }).nodeify(callback);
-    else
-      return exec().nodeify(callback);
+        .then(function () {
+          return exec();
+        })
+        .nodeify(callback);
+    } else return exec().nodeify(callback);
   },
 
   addMigrationRecord: function (name, callback) {
     var formattedDate = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
-    this.runSql('INSERT INTO ' + internals.migrationTable + ' (`name`, `run_on`) VALUES (?, ?)', [name, formattedDate], callback);
+    this.runSql(
+      'INSERT INTO ' +
+        internals.migrationTable +
+        ' (`name`, `run_on`) VALUES (?, ?)',
+      [name, formattedDate],
+      callback
+    );
   },
 
   addSeedRecord: function (name, callback) {
     var formattedDate = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
-    this.runSql('INSERT INTO ' + internals.seedTable + ' (`name`, `run_on`) VALUES (?, ?)', [name, formattedDate], callback);
+    this.runSql(
+      'INSERT INTO ' +
+        internals.seedTable +
+        ' (`name`, `run_on`) VALUES (?, ?)',
+      [name, formattedDate],
+      callback
+    );
   },
 
-  addForeignKey: function(tableName, referencedTableName, keyName, fieldMapping, rules, callback) {
-    if(arguments.length === 5 && typeof(rules) === 'function') {
+  addForeignKey: function (
+    tableName,
+    referencedTableName,
+    keyName,
+    fieldMapping,
+    rules,
+    callback
+  ) {
+    if (arguments.length === 5 && typeof rules === 'function') {
       callback = rules;
       rules = {};
     }
     var columns = Object.keys(fieldMapping);
-    var referencedColumns = columns.map(function (key) { return fieldMapping[key]; });
-    var sql = util.format('ALTER TABLE `%s` ADD CONSTRAINT `%s` FOREIGN KEY (%s) REFERENCES `%s` (%s) ON DELETE %s ON UPDATE %s',
-      tableName, keyName, this.quoteDDLArr( columns ), referencedTableName,
-      this.quoteDDLArr( referencedColumns ), rules.onDelete || 'NO ACTION', rules.onUpdate || 'NO ACTION');
+    var referencedColumns = columns.map(function (key) {
+      return fieldMapping[key];
+    });
+    var sql = util.format(
+      'ALTER TABLE `%s` ADD CONSTRAINT `%s` FOREIGN KEY (%s) REFERENCES `%s` (%s) ON DELETE %s ON UPDATE %s',
+      tableName,
+      keyName,
+      this.quoteDDLArr(columns),
+      referencedTableName,
+      this.quoteDDLArr(referencedColumns),
+      rules.onDelete || 'NO ACTION',
+      rules.onUpdate || 'NO ACTION'
+    );
 
     return this.runSql(sql).nodeify(callback);
   },
 
-  removeForeignKey: function(tableName, keyName, options, callback) {
-    var sql = util.format('ALTER TABLE `%s` DROP FOREIGN KEY `%s`', tableName,
-      keyName);
+  removeForeignKey: function (tableName, keyName, options, callback) {
+    var sql = util.format(
+      'ALTER TABLE `%s` DROP FOREIGN KEY `%s`',
+      tableName,
+      keyName
+    );
 
-    if(typeof(options) === 'function') {
-
+    if (typeof options === 'function') {
       callback = options;
     }
 
     return this.runSql(sql)
-    .then(function () {
-
-      if(options && options.dropIndex === true) {
-
-        sql = util.format('ALTER TABLE `%s` DROP INDEX `%s`',
-          tableName, keyName);
-        return this.runSql(sql);
-      }
-      else {
-
-        return Promise.resolve();
-      }
-
-    }.bind(this)).nodeify(callback);
+      .then(
+        function () {
+          if (options && options.dropIndex === true) {
+            sql = util.format(
+              'ALTER TABLE `%s` DROP INDEX `%s`',
+              tableName,
+              keyName
+            );
+            return this.runSql(sql);
+          } else {
+            return Promise.resolve();
+          }
+        }.bind(this)
+      )
+      .nodeify(callback);
   },
 
-  runSql: function() {
-
+  runSql: function () {
     var self = this;
     var args = this._makeParamArgs(arguments);
 
     var callback = args.pop();
     log.sql.apply(null, arguments);
-    if(internals.dryRun) {
+    if (internals.dryRun) {
       return Promise.resolve().nodeify(callback);
     }
 
-    return new Promise(function(resolve, reject) {
-      args.push(function(err, data) {
-        return (err ? reject(err) : resolve(data));
+    return new Promise(function (resolve, reject) {
+      args.push(function (err, data) {
+        return err ? reject(err) : resolve(data);
       });
 
       self.connection.query.apply(self.connection, args);
     }).nodeify(callback);
   },
 
-  _makeParamArgs: function(args) {
+  _makeParamArgs: function (args) {
     var params = Array.prototype.slice.call(args);
     var sql = params.shift();
     var callback = params.pop();
@@ -394,7 +437,7 @@ var MysqlDriver = Base.extend({
     return [sql, params, callback];
   },
 
-  all: function() {
+  all: function () {
     var args = this._makeParamArgs(arguments);
 
     log.sql.apply(null, arguments);
@@ -402,26 +445,26 @@ var MysqlDriver = Base.extend({
     return this.connection.query.apply(this.connection, args);
   },
 
-  close: function(callback) {
-    return new Promise(function(resolve, reject) {
-      var cb = (function(err, data) {
-        return (err ? reject(err) : resolve(data));
-      });
+  close: function (callback) {
+    return new Promise(
+      function (resolve, reject) {
+        var cb = function (err, data) {
+          return err ? reject(err) : resolve(data);
+        };
 
-      this.connection.end(cb);
-    }.bind(this)).nodeify(callback);
+        this.connection.end(cb);
+      }.bind(this)
+    ).nodeify(callback);
   }
-
 });
 
 Promise.promisifyAll(MysqlDriver);
 
-function dummy() {
-
+function dummy () {
   arguments[arguments.length - 1]('not implemented');
 }
 
-exports.connect = function(config, intern, callback) {
+exports.connect = function (config, intern, callback) {
   var db;
 
   internals = intern;
@@ -430,18 +473,17 @@ exports.connect = function(config, intern, callback) {
 
   internals.interfaces.SeederInterface._makeParamArgs = dummy;
 
-  if (typeof(mysql.createConnection) === 'undefined') {
+  if (typeof mysql.createConnection === 'undefined') {
     db = config.db || new mysql.createClient(config);
   } else {
     db = config.db || new mysql.createConnection(config);
   }
 
-  db.connect(function( err ) {
-
-    if( err ) {
-      return callback( err );
+  db.connect(function (err) {
+    if (err) {
+      return callback(err);
     }
 
     callback(null, new MysqlDriver(db));
-  } );
+  });
 };
